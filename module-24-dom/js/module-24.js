efe71201cdad77462565e04c2a61d43f4515414e
ahsan1999wd <ahsan1999wd@gmail.com>
2022-08-15 23:21:39 +0600

    new file added

---
+++ module-24-dom/js/module-24.js
@@ -0,0 +1,39 @@
+/********** !!!-----DOM-----!!! **********/
+
+/********** Document-Object-MODEL **********/
+
+/************************
+
+
+option-1: getElementsByTagName <-----!!!
+
+option-2: getElementById <-----------!!!
+
+option-3: getElementsByClassName <---!!!
+
+option-4: querySelector (first matching)<---------!!!
+
+option-5: querySelectorAll (node list)  <---------!!!
+
+* title.classList   <-----------!!!
+
+* title.classList.remove('') <--!!!
+
+* title.classList.add('') <-----!!!
+
+title.setAttribute('', '')   <----------!!!
+
+\n <---meaning new line-----> !!!
+
+.innerHTML (comment/tag/text/all)  <-----!!!
+
+.innerText (text only)  <-----!!!
+
+.parentNode (like up) <-----!!!
+
+.childNode (like down) <-----!!!
+
+.append (something add / push) <-----!!!
+
+
+*************************/
